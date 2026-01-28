6d0bf691eccfa0aa41ad3954c8ee22ac741fe81f
camaraballamoussa <ballamoussacamara86@gmail.com>
2026-01-28 20:01:17 +0000

    first comit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Layout({ children }:{ children:React.ReactNode }){
+    return (
+        <div className="flex h-screen flex-col md:flex-row md:overfolw-hidden">
+            <div>
+                <SideNav/>
+            </div>
+            <div className="grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
+        </div>
+    );
+}
